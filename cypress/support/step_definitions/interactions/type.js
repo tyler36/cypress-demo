c16231cbefaa7943241d8844231efbee968061e9
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-08-27 14:11:07 +0900

    reverse values for JA locale

--- cypress/support/step_definitions/interactions/type.js
+++ cypress/support/step_definitions/interactions/type.js
@@ -1,7 +1,11 @@
 import { Then } from '@badeball/cypress-cucumber-preprocessor'
 
 Then('I type {string} into {string}', cy.typeStringInto)
-Then('{string}に{string}と入力します', cy.typeStringInto)
+Then('{string}に{string}と入力します', (name, query) =>
+  cy.typeStringInto(query, name),
+)
 
 Then('I cktype {string} into {string}', cy.typeStringIntoCkeditor)
-Then('{string}エディターに{string}と入力します', cy.typeStringIntoCkeditor)
+Then('{string}エディターに{string}と入力します', (name, query) =>
+  cy.typeStringIntoCkeditor(query, name),
+)
