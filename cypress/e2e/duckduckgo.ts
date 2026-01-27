f9004a72de77dbdcbf7a361fb64358d5b6d25faf
tyler36 <7234392+tyler36@users.noreply.github.com>
2026-01-27 09:58:48 +0900

    fix: update start page and selector

--- cypress/e2e/duckduckgo.ts
+++ cypress/e2e/duckduckgo.ts
@@ -1,11 +1,11 @@
 import { When, Then } from '@badeball/cypress-cucumber-preprocessor'
 
 When('I visit duckduckgo.com', () => {
-  cy.visit('https://www.duckduckgo.com')
+  cy.visit('https://start.duckduckgo.com/')
 })
 
 Then('I should see a search bar', () => {
-  cy.get('#searchbox_input').should(
+  cy.get('[data-ssg-id="ai-searchbox-input"]').should(
     'have.attr',
     'aria-label',
     'Search with DuckDuckGo',
